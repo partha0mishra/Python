8387f1cead038a9e8610d469f8e261b759354c1a
Partha Mishra <partha.mishra@gmail.com>
2025-01-16 13:48:37 +1100

    variables

---
+++ PCEP/pcep-codes/00/007_Variables.py
@@ -0,0 +1,11 @@
+var = 1
+print(var)
+var = var +100
+print(var)
+
+account_balance = 1000.0
+client_name = "Kash Patel"
+print(var, account_balance, client_name)
+
+var="3.8.5"
+print("Python Version: "+var)
