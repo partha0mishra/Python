f873f72d6b7c0b4652b8c07843c12427f67a18f9
Partha Mishra <partha.mishra@gmail.com>
2025-01-21 13:19:37 +1100

    more learning

---
+++ PCEP/pcep-codes/00/013_input_cast.py
@@ -0,0 +1,2 @@
+number = float(input("Enter a number:"))
+print("Double of ",number,"is", number*2)
---
+++ PCEP/pcep-codes/00/014_input_calcualte_hypo.py
@@ -0,0 +1,3 @@
+side1=float(input("Enter value of first side:"))
+side2=float(input("Enter value of second side:"))
+print("The three sides are: ",side1,",", side2, "and", (side1**2+side2**2)**0.5)
